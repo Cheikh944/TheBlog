cf2854be0e61f5d0061bb9dbbabfef65186d21fc
Cheikh944 <cheikhdiag@yahoo.com>
2024-01-17 16:43:06 +0100

    Firebase auth

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -44,6 +44,49 @@ const LoginUser = async (req, res)=>{
     }
 }
 
+const ProviderAuth = async (req, res)=>{
+    const { content } = req.body;
+    const name = content.user.displayName;
+    const password = content.user.email + content.user.uid;
+    const user = await User.findOne({ name });
+    if(user){
+        const match = await bcrypt.compare(password, user.password);
+        if (match){
+            const accessToken = jwt.sign({ id: user._id, name}, process.env.ACCESS_KEY,{expiresIn: '1h' });
+            const refreshToken = jwt.sign({ id: user._id}, process.env.REFRESH_KEY,{expiresIn: '5d' });
+            res.cookie('jwt', refreshToken, {
+                httpOnly : true,
+                maxAge:  5 * 24 * 60 * 60 * 1000,
+                path: '/',
+                secure: true,
+                sameSite: 'None'
+            })
+            res.json({ name , id: user._id,  accessToken })
+        }else{
+            return res.status(500).json({ success: false, error: 'Erreur de connexion' });
+        }
+    }else{
+        const hash = await bcrypt.hash(password, 10);
+        const newUser = new User({ name, password : hash });
+        await newUser.save();
+        const loginUser = await User.findOne({ name });
+        const match = await bcrypt.compare(password, loginUser.password);
+        if (match){
+            const accessToken = jwt.sign({ id: loginUser._id, name}, process.env.ACCESS_KEY,{expiresIn: '1h' });
+            const refreshToken = jwt.sign({ id: loginUser._id}, process.env.REFRESH_KEY,{expiresIn: '5d' });
+            res.cookie('jwt', refreshToken, {
+                httpOnly : true,
+                maxAge:  5 * 24 * 60 * 60 * 1000,
+                path: '/',
+                secure: true,
+                sameSite: 'None'
+            })
+            res.json({ name , id: loginUser._id,  accessToken })
+        }
+    }
+}
+
+
 const RefreshUser= (req, res) =>{
     const cookie = req.cookies
     if(!cookie?.jwt) return res.status(401).json({error: 'Unauthorized'})
@@ -65,7 +108,7 @@ const Logout = (req, res) => {
     if (!cookie?.jwt) {
         return res.status(401).json({ error: 'Unauthorized' });
     }
-    res.clearCookie('jwt', { httpOnly: true, secure: true, sameSite: 'None' });
+    res.clearCookie('jwt', { httpOnly: true, secure: true, sameSite: 'None'});
     res.status(204).end();
 };
 
@@ -73,5 +116,6 @@ module.exports = {
     RegisterUser,
     LoginUser,
     RefreshUser,
-    Logout
+    Logout, 
+    ProviderAuth
 }
--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -4,6 +4,7 @@ const UserController = require('../controllers/UserController')
 
 router.post("/register", UserController.RegisterUser);
 router.post("/login", UserController.LoginUser);
+router.post("/provider/auth", UserController.ProviderAuth);
 router.get("/refresh", UserController.RefreshUser);
 router.get("/logout", UserController.Logout);
 
